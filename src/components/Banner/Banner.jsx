3ac5e9fb2f6ada91e0d89ce1c369320ab7b94757
Ayan Sujon <ayansujonbd@gmail.com>
2025-10-04 23:22:08 +0600

    added card

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import bookImage from "../../assets/books.jpg";
+import bookImage from "../../assets/book1.png";
 const Banner = () => {
     return (
-        <div className='flex justify-around items-center p-16 w-full bg-amber-50 '>
+        <div className="hero bg-base-200 min-h-screen md:p-20">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+            src={bookImage}
+            className="max-w-sm rounded-lg "
+            />
             <div>
-                <h1>Books to freshen up your bookshelf</h1>
-                <button className='btn btn-primary'>View The List</button>
+            <h1 className="text-5xl font-bold mb-4">Books to freshen up your bookshelf</h1>
+            <button className="btn btn-primary">View The List</button>
             </div>
-            <div>
-                <figure className='w-4/12'>
-                    <img src={bookImage} alt="Banner Image" />
-                </figure>
-            </div>
+        </div>
         </div>
     );
 };
